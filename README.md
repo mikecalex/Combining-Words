9dcda3bc0c81e7989ede2f59bb77d885441d2f09
mikecalex <mikecalex@gmail.com>
2017-12-16 22:23:23 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic Ruby program that takes individual words from users and concats them to make a sentence.
---
+++ code.rb
@@ -0,0 +1,14 @@
+print "Enter the first word: "
+wordOne = gets.chomp
+print "Enter the second word: "
+wordTwo = gets.chomp
+print "Enter the third word: "
+wordThree = gets.chomp
+print "Enter the fourth word: "
+wordFour = gets.chomp
+print "Enter the fifth word: "
+wordFive = gets.chomp
+print "Enter the sixth word: "
+wordSix = gets.chomp
+
+puts wordOne + " " + wordTwo + " " + wordThree + " " + wordFour + " " + wordFive + " " + wordSix + "."
